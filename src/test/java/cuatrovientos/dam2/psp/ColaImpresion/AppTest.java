773c57c11dd63fa2504164f43ea23914da1423c6
Javier <jrenzullis@gmail.com>
2026-02-01 14:05:18 +0100

    proyecto y topics creados

---
+++ src/test/java/cuatrovientos/dam2/psp/ColaImpresion/AppTest.java
@@ -0,0 +1,19 @@
+package cuatrovientos.dam2.psp.ColaImpresion;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
